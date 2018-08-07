df1146ea8dd251c0ba33eb2e517de1dcb4783ead
Jeff Hollan <jehollan@microsoft.com>
2018-08-06 23:32:18 -0700

    updating getKeys.ps1 to take and write variables

--- CSharpOddOrEven/getKeys.ps1
+++ CSharpOddOrEven/getKeys.ps1
@@ -1,3 +1,9 @@
+Param(
+	[string]$appName,
+	[string]$functionName,
+	[string]$resourceGroup
+)
+
 $publish = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroup -ResourceType Microsoft.Web/sites/config -ResourceName "$($appName)/publishingcredentials" -Action list -ApiVersion 2015-08-01 -Force
 $username = $publish.properties.publishingUserName
 $password = $publish.properties.publishingPassword
@@ -5,4 +11,7 @@ $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0
 
 $scmUrl = "https://$($siteName).scm.azurewebsites.net"
 $token = Invoke-RestMethod -Uri "https://$($appName).scm.azurewebsites.net/api/functions/admin/token" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method GET
-$keys = Invoke-RestMethod -Uri "https://$($appName).azurewebsites.net/admin/functions/$($functionName)/keys" -Headers @{Authorization=("Bearer {0}" -f $token)} -Method GET
+$keyResponse = Invoke-RestMethod -Uri "https://$($appName).azurewebsites.net/admin/functions/$($functionName)/keys" -Headers @{Authorization=("Bearer {0}" -f $token)} -Method GET
+$key = $keyResponse.keys[0].value
+
+Write-Host "##vso[task.setvariable variable=functionUrl]https://$($appName).azurewebsites.net/api/$($functionName)&code=$($key)"
